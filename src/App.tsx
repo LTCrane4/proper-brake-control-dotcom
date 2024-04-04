d5f487798d58dfa34ce02cdcc2766a848b423941
Teddy Crane <teddy.crane@slalom.com>
2024-04-04 09:29:31 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css'
+import HomePage from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/test",
+      element: <div>You found the secret page!</div>,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <React.StrictMode>
+      <h1>Proper Brake Control</h1>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export const HomePage = (): JSX.Element => {
+    return (<div>Content coming soon!</div>);
+};
+
+export default HomePage;
