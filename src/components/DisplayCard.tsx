bee0f6d6ca99323f614a6a2636cc7bccd41153fe
Teddy Crane <teddy.crane@slalom.com>
2024-04-04 13:04:27 -0700

    added iframe link to placeholder youtube

---
+++ src/components/DisplayCard.tsx
@@ -0,0 +1,21 @@
+import { Box, Card, CardContent, Typography } from "@mui/material";
+
+export type DisplayCardProps = {
+    title?: string;
+    content?: any;
+};
+
+export const DisplayCard = ({ title, content }: DisplayCardProps): JSX.Element => {
+    return (
+        <Box sx={{ margin: '4rem', height: '90rem' }}>
+            <Card sx={{ height: '90rem' }}>
+                <CardContent sx={{ height: '100%', marginBottom: '2rem' }}>
+                    {title ? <Typography variant="h6">{title}</Typography> : null}
+                    {content ?? null}
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
+
+export default DisplayCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { Typography } from "@mui/material";
+import DisplayCard from "../components/DisplayCard";
 
 export const HomePage = (): JSX.Element => {
-    return (<div><Typography variant="h6">Content coming soon!</Typography></div>);
+    return (<div><Typography variant="h6">Content coming soon!</Typography>
+        <DisplayCard content={<iframe width="100%" height="97%" src="https://www.youtube.com/embed/1OG1KEjJu48?si=ODvUNIzow3d-kWiJ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>} />
+    </div>);
 };
 
 export default HomePage;
